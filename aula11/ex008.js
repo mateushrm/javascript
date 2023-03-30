1653f5f94878aac533d0365955d0da85067d2dce
Mateus Matos <mateusmatos2004@gmail.com>
2023-03-30 14:43:49 -0300

    exercícios de condições

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 70.5
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+
+if(vel > 80){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+
+console.log('Dirija sempre usando cinto de segurança')
