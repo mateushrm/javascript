b51da2215bfda3b01f898aed063ea7f8523de363
Mateus Matos <mateusmatos2004@gmail.com>
2023-04-05 14:21:54 -0300

    repetições (while, do while)

---
+++ aula13/ambiente.js
@@ -0,0 +1,25 @@
+var cont = 1
+do{
+    console.log(`Passo ${cont}`)
+    cont++
+}while(cont <= 10)
+
+/*while(cont <= 10){
+    console.log(`Passo ${cont}`)
+    cont++
+}*/
+
+/* 
+console.log('Passo 1')
+console.log('Passo 2')
+console.log('Passo 3')
+console.log('Passo 4')
+console.log('Passo 5')
+console.log('Passo 6')
+console.log('Passo 7')
+console.log('Passo 8')
+console.log('Passo 9')
+console.log('Passo 10')
+*/
+
+
