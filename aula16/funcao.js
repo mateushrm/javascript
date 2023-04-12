0567c92d57ca05f7e651b67296f1035018cc7f1b
Mateus Matos <mateusmatos2004@gmail.com>
2023-04-12 14:36:54 -0300

    exercícios de funções

---
+++ aula16/funcao.js
@@ -0,0 +1,10 @@
+function parimp(n){
+    if (n%2==0){
+        return 'Par!'
+    } else{
+        return 'Impar!'
+    }
+}
+
+let res = parimp(5) //console.log(parimp(5))
+console.log(res)
---
+++ aula16/funcao3.js
@@ -0,0 +1,4 @@
+let v = function(x){
+    return x*3
+}
+console.log(v(5))
